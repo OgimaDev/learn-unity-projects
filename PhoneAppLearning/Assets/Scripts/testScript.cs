39e448394a95186c3a34f5ff66ec2fd138ae7bdc
jacod <77664513+OgimaDev@users.noreply.github.com>
2021-05-09 09:39:02 -0500

    final projects
    Projects

---
+++ PhoneAppLearning/Assets/Scripts/testScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class testScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Did this work?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
